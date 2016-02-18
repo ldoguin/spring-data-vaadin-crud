981d86573ed6b558d1699ee8497ff73740dacbbc
Laurent Doguin <laurent.doguin@couchbase.com>
2016-02-18 12:45:59 +0100

    add backend validation listener

--- src/main/java/crud/Application.java
+++ src/main/java/crud/Application.java
@@ -1,8 +1,12 @@
 package crud;
 
+import javax.validation.Validator;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 // Enable additional servlet filters for wscdn and cloud hosted fontawesome
@@ -13,4 +17,9 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
     
+	@Bean
+	public Validator validator() {
+		return new LocalValidatorFactoryBean();
+	}
+
 }
